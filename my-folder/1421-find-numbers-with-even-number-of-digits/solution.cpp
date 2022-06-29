134ab2ab36c2cd775b66c1d69e1db87d3e2db2de
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2022-06-29 14:26:08 +0000

    [LeetCode Sync] Runtime - 7 ms (26.51%), Memory - 10.1 MB (100.00%)

--- my-folder/1421-find-numbers-with-even-number-of-digits/solution.cpp
+++ my-folder/1421-find-numbers-with-even-number-of-digits/solution.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    int findNumbers(vector<int>& nums) {
+   /* int findNumbers(vector<int>& nums) {
         vector <string> str;
         int count=0;
         for(int i:nums) str.push_back(to_string(i));
         for(string s:str)
             if(s.length()%2==0) count++;
         return count;
+    }*/
+    int findNumbers(vector<int>& nums) {
+        int res=0;
+        for(auto num:nums) {
+            res+=(int)log10(num)&1;
+        }
+        return res;
     }
 };
