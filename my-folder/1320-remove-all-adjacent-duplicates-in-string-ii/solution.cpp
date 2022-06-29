5ea62a36f1f75028d3c2892bc6763eea6815be0b
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2022-06-29 14:24:49 +0000

    [LeetCode Sync] Runtime - 22 ms (51.13%), Memory - 11 MB (91.30%)

--- my-folder/1320-remove-all-adjacent-duplicates-in-string-ii/solution.cpp
+++ my-folder/1320-remove-all-adjacent-duplicates-in-string-ii/solution.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
-
     string removeDuplicates(string s, int k) {
+        int i = 0, n = s.length();
+        vector<int> count(n);
+        for (int j = 0; j < n; ++j, ++i) {
+            s[i] = s[j];
+            count[i] = i > 0 && s[i - 1] == s[j] ? count[i - 1] + 1 : 1;
+            if (count[i] == k) i -= k;
+        }
+        return s.substr(0, i);
+    }
+    /*string removeDuplicates(string s, int k) {
         int n = s.size();
         if(n<k) return s;
         
@@ -26,7 +35,7 @@ public:
         }
         reverse(ans.begin(), ans.end());
         return ans;
-    }
+    }*/
     
 };
 
