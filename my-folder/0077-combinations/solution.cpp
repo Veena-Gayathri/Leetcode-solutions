7d57e9d1113f8c476ff89600903f3d81e5a2ea8d
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2022-08-06 11:23:50 +0000

    [LeetCode Sync] Runtime - 43 ms (100.00%), Memory - 17.9 MB (100.00%)

--- my-folder/0077-combinations/solution.cpp
+++ my-folder/0077-combinations/solution.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    void generate(int ind,vector<int>& otp,vector<int> &nums, int n,int k,vector<vector<int>> &op, int size){
-        if(ind==n){
-            if(size==k){ 
-                op.push_back(otp);
-            }
-            return;
-        }
-        otp.push_back(nums[ind]);
-        generate(ind+1,otp,nums,n,k,op,size+1);
-        otp.pop_back();
-        generate(ind+1,otp,nums,n,k,op,size);
-        return;
+    void answer(int k,int start,int end,vector<vector<int>> &combination,vector<int> &p,int size){
+        if(start==end+1) {if(size==k) { combination.push_back(p); } return;}
+        p.push_back(start); 
+        answer(k,start+1,end,combination,p,size+1);
+        p.pop_back(); 
+        answer(k,start+1,end,combination,p,size);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums;
-        vector <int> otp;
-        int ind;
-        int size;
-        for(int i=1;i<=n;i++) nums.push_back(i);
-        vector<vector<int>> op;
-        generate(0,otp,nums,n,k,op,0);
-        return op;
+        int start=1,end=n,size=0;
+        vector<int> p;
+        vector <vector<int>> combination;
+        answer(k,start,end,combination,p,size);
+        return combination;
     }
 };
