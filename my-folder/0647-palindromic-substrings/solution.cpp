1b2c570742d5777521c524e041acbd8910a38f76
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2022-08-30 13:33:29 +0000

    [LeetCode Sync] Runtime - 15 ms (43.48%), Memory - 10.5 MB (49.22%)

--- my-folder/0647-palindromic-substrings/solution.cpp
+++ my-folder/0647-palindromic-substrings/solution.cpp
@@ -2,35 +2,21 @@ class Solution {
     public:
     int countSubstrings(string s) {
 	int n = s.size();
-
-	// dp[left][right] represent whether the substring [left,right] is palindromic or not
-	// row index is the left index of substring, column index is the right index of substring
 	int dp[n][n];
-
-	// init the matrix with zero
 	memset(dp, 0, sizeof(dp));
-
 	int count = 0;
-
-	// each character in the string is a palindromic string, so set dp[i][i] to 1
 	for (int i = 0; i < n; i++) {
 		dp[i][i] = 1;
 		count++;
 	}
-
-
 	for (int right = 1; right < n; right++) {
 		for (int left = 0; left < right; left++) {
-			// a substring is palindromic if the characters at left index and right index are equal and:
-			// 1) left and right index are next to each other OR
-			// 2) the substring [left+1,right-1] is palindromic (which was already determined)
-			if (s[left] == s[right] && (left+1==right || dp[left+1][right-1])) {
+			if (s[left] == s[right] && (left+1==right || dp[left+1][right-1])){
 				dp[left][right] = 1;
 				count++;
 			}
 		}
 	}
-
 	return count;
 }
     
