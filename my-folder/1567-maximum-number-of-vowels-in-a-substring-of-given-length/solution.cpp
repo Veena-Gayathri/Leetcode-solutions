1845f8a61ebcc73fc54b24b7707a3555594397c4
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2024-04-13 19:56:22 +0000

    [LeetCode Sync] Runtime - 11 ms (98.32%), Memory - 11.2 MB (71.83%)

--- my-folder/1567-maximum-number-of-vowels-in-a-substring-of-given-length/solution.cpp
+++ my-folder/1567-maximum-number-of-vowels-in-a-substring-of-given-length/solution.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    int maxVowels(string s, int k) {
-        int n=s.length(),i=0,op=0,res=0;
-        unordered_map <char,int> mp;
-        mp['a']=1; mp['e']=1; mp['i']=1;mp['o']=1;mp['u']=1;
-        string str=s.substr(0,k);
-        for(int j=0;j<k;j++){
-                op+=mp[str[j]];
-            }
-        while(i+k<=n){
-            res=max(op,res);
-            op-=mp[s[i]]; op+=mp[s[i+k]];
-            i++; 
-        }
-        return res;
-    }
 
+    bool isVowel(char c){
+        if(c== 'A' || c== 'a' || c== 'E' || c== 'e'|| c== 'I'|| c== 'i'|| c== 'O'|| c== 'o'|| c== 'U'|| c== 'u'){
+            return true;
+        }
+        return false;
+    }
+    int maxVowels(string s, int k) {
+        int pt1=0;
+        int pt2= k-1;
+        int count=0;
+        int max_count=0;
+        cout<<s.length()<<endl;
+        if(s.size()<k) {
+            for(auto it:s){
+                count+= (isVowel(it))? 1:0;
+            }
+            return count;
+        }
+        for(int i=pt1; i<=pt2; i++){
+            count+= (isVowel(s[i]))? 1:0;
+        }
+        max_count=count;
+        while(pt2<s.size()){
+            // for(int i=pt1; i<= pt2; i++){
+            //     count+= (isVowel(s[i]))? 1:0;
+            // } 
+            if(isVowel(s[pt1]) ) count--;
+            if(isVowel(s[pt2+1]) ) count++;
+            pt1++;
+            pt2++;
+            max_count= max(max_count, count);
+            // count =0;
+        }
+        return max_count;
+    }
 };
