5be49032547e8eb652d5e1e9992d4a93409c7db9
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2024-04-13 17:46:27 +0000

    [LeetCode Sync] Runtime - 28 ms (10.33%), Memory - 21.7 MB (37.20%)

--- my-folder/0283-move-zeroes/solution.cpp
+++ my-folder/0283-move-zeroes/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        fill(remove(nums.begin(), nums.end(),0), nums.end(), 0);
+        int t=nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                // cout<<nums[i]<<endl;
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+        for(int i=nums.size()-1; i< t-1; i++){
+            nums.push_back(0);
+            // for(auto it:nums){
+            //     cout<<it;
+            // }
+        }
     }
 };
