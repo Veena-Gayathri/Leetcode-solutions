3a9ac956d16af34a09d181c8e1c83249d09b3336
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2026-04-14 16:04:17 +0000

    [LeetCode Sync] Runtime - 99 ms (69.59%), Memory - 26.5 MB (11.76%)

--- my-folder/1370-count-number-of-nice-subarrays/solution.py
+++ my-folder/1370-count-number-of-nice-subarrays/solution.py
@@ -1,23 +1,33 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+
+
         nums = [1 if x % 2 else 0 for x in nums]
+        pri = {0:1}  # prefix sum counts
+        res = 0
+        summ = 0
+        for i in nums:
+            summ += i
+            res += pri.get(summ - k, 0)
+            pri[summ] = pri.get(summ, 0) + 1
+        return res
         
-        def atMost(k):
-            left = 0
-            count = 0
-            res = 0
+        # def atMost(k):
+        #     left = 0
+        #     count = 0
+        #     res = 0
             
-            for right in range(len(nums)):
-                if nums[right] == 1:
-                    count += 1
+        #     for right in range(len(nums)):
+        #         if nums[right] == 1:
+        #             count += 1
                 
-                while count > k:
-                    if nums[left] == 1:
-                        count -= 1
-                    left += 1
+        #         while count > k:
+        #             if nums[left] == 1:
+        #                 count -= 1
+        #             left += 1
                 
-                res += (right - left + 1)
+        #         res += (right - left + 1)
             
-            return res
+        #     return res
         
-        return atMost(k) - atMost(k - 1)
+        # return atMost(k) - atMost(k - 1)
