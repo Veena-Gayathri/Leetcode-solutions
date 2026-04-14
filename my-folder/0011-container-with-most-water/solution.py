a0140673d8b8cf7b7db5d04ef49c03eb42bec941
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2026-04-14 17:15:10 +0000

    [LeetCode Sync] Runtime - 55 ms (73.16%), Memory - 29.5 MB (67.89%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -13,3 +13,49 @@ class Solution:
                 right -= 1
         
         return max_area
+
+
+
+"""
+ 0 1 2 3 4 5 6 7 8
+[1,8,6,2,5,4,8,3,7]
+
+1: start= 0 end = 8
+    water = end - start * min(height) = 8
+    max_water = 8
+    which is min, start +=1
+2: start = 1, end = 8
+    water = 7* 7 = 49
+    max_water = 49
+    end -= 1
+3: 4: start = 1, end = 6
+    water = 5* 8 = 40
+    max_water = 49
+    start +=1 and end-=1
+left< right: condition breaks, max_water = 49
+"""
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
