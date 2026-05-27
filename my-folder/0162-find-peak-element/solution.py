d9ce7bdd24469e68baefe841e1a15c1cf5e8d325
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2026-05-27 12:30:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (39.44%)

--- my-folder/0162-find-peak-element/solution.py
+++ my-folder/0162-find-peak-element/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
-        left, right = 0, len(nums) - 1
+        left, right = 0, len(nums)-1
         
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] < nums[mid + 1]:
-                # peak is on the right
-                left = mid + 1
+        while left< right:
+
+            mid = left + (right - left)//2
+            if nums[mid+1] > nums[mid] :
+                left = mid+1
             else:
-                # peak is on the left or mid
                 right = mid
-        
+
         return left
-        
+            
+
