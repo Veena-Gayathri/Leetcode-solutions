d4d0103a4610d6ecbd4a82611dedc08e04d3f676
Veena Gayathri Tamma <65124863+Veena-Gayathri@users.noreply.github.com>
2026-07-03 05:00:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (18.63%)

--- my-folder/0050-powx-n/solution.py
+++ my-folder/0050-powx-n/solution.py
@@ -1,11 +1,36 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n ==0:
-            return 1.0
-        if n<0:
-            return self.myPow(1/x,-n)
-        if n%2==0:
-            return self.myPow(x*x,n//2)
-        else:
-            return x*self.myPow(x*x,n//2)
-        
+            if n == 0:
+                return 1.0
+            if n<0:
+                return self.myPow(1/x, -n)
+            if n %2 == 0:
+                return self.myPow(x*x, n/2) 
+            if n%2 ==1:
+                return x * self.myPow(x*x, n//2) 
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
